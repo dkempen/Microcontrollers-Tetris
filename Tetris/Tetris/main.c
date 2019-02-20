904658b41f7d588c1a7ea177a9f8c00f622a4289
dkempen <daan.vankempen2@gmail.com>
2019-02-20 13:55:29 +0100

    [Added] Base project + .gitignore

---
+++ Tetris/Tetris/main.c
@@ -0,0 +1,16 @@
+/*
+ * Tetris.c
+ *
+ * Created: 20-Feb-19 13:50:34
+ * Author : Daan, Ian, Max, Jordy
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
